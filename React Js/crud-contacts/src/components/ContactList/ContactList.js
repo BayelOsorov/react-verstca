c7d15efdcb6ff0becdec8cb6a7e3743958a947b4
BayelOsorov <osorov.bayel@gmail.com>
2021-10-26 23:42:22 +0600

    contacts crud

---
+++ React Js/crud-contacts/src/components/ContactList/ContactList.js
@@ -0,0 +1,30 @@
+
+import { Card, Button } from 'react-bootstrap';
+
+const ContactList = (props) => {
+
+    return (
+        <div className='container d-flex flex-wrap '>
+
+            {
+                props.contacts.map((item, index) => (
+                    <Card
+                        className='cards'
+                        key={item.id} style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src={item.img} />
+                        <Card.Body>
+                            <Card.Title>{item.name}</Card.Title>
+                            <Card.Text>{item.phone}</Card.Text>
+                            <Button className='btn-card' variant="danger" onClick={() => props.handleDeleteContact(item.id)}>Delete</Button>
+                            <Button onClick={() => props.handleEditIndex(index)}>Edit Contact</Button>
+                        </Card.Body>
+                    </Card>
+
+                ))
+            }
+
+        </div>
+    );
+};
+
+export default ContactList;
