4b792a9289e9abbee92d04a75507c17673ae0c8f
BayelOsorov <osorov.bayel@gmail.com>
2021-12-01 14:58:24 +0600

    gg

--- main-project/src/helpers/const.js
+++
@@ -1,6 +0,0 @@
-export const API = 'http://localhost:8000/phones'
-export const USER_API = 'http://localhost:8000/users'
-export const COMMENTS_API = 'http://localhost:8000/comments'
-export const LIKES_API = 'http://localhost:8000/likes'
-
-
